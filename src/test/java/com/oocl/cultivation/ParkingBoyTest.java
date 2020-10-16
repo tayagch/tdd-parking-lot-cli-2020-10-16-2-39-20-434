edd189553a0e0014c037628802e26de8d8887f34
TAYAGCH <tayagch@oocl.com>
2020-10-16 14:04:46 +0800

    [ParkingLot][Christian] test : implement case when parking boy park a car into parking lot then should return a parkingTicket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,19 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    public void should_return_parkingTicket_when_parkingBoy_park_car_given_parkingBoy_a_car(){
+        // Given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
 
+        // When
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        // Then
+        assertNotNull(parkingTicket);
     }
 }
