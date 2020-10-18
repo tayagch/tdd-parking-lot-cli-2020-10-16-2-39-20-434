a7a3d72c0721b62bda61f4c83c9f67227b8aece9
TAYAGCH <tayagch@oocl.com>
2020-10-18 21:20:48 +0800

    [ParkingLot][Christian] refactor : create setup method for inital setup of management list

--- src/test/java/com/oocl/cultivation/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -1,5 +1,6 @@
 package com.oocl.cultivation;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,22 +10,45 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingManagerTest {
+
+    List<ParkingLot> parkingLots1;
+    List<ParkingLot> parkingLots2;
+    List<ParkingLot> parkingLots3;
+    ParkingBoy parkingBoy1;
+    ParkingBoy parkingBoy2;
+    SmartParkingBoy smartParkingBoy1;
+    SmartParkingBoy smartParkingBoy2;
+    SuperSmartParkingBoy superSmartParkingBoy1;
+    SuperSmartParkingBoy superSmartParkingBoy2;
+
+    List<ParkingBoy> parkingBoyList;
+    List<SmartParkingBoy> smartParkingBoyList;
+    List<SuperSmartParkingBoy> superSmartParkingBoyList;
+
+    @Before
+    public void setup(){
+        parkingLots1 = Arrays.asList(new ParkingLot(10),new ParkingLot(10));
+        parkingLots2 = Arrays.asList(new ParkingLot(10),new ParkingLot(15));
+        parkingLots3 = Arrays.asList(new ParkingLot(15),new ParkingLot(10));
+
+        parkingBoy1 = new ParkingBoy(parkingLots1);
+        parkingBoy2 = new ParkingBoy(parkingLots1);
+
+        smartParkingBoy1 = new SmartParkingBoy(parkingLots2);
+        smartParkingBoy2 = new SmartParkingBoy(parkingLots2);
+
+        superSmartParkingBoy1 = new SuperSmartParkingBoy(parkingLots3);
+        superSmartParkingBoy2 = new SuperSmartParkingBoy(parkingLots3);
+
+        parkingBoyList = Arrays.asList(parkingBoy1, parkingBoy2);
+        smartParkingBoyList = Arrays.asList(smartParkingBoy1,smartParkingBoy2);
+        superSmartParkingBoyList = Arrays.asList(superSmartParkingBoy1,superSmartParkingBoy2);
+    }
+
     @Test
     public void should_return_management_list_when_parkingManager_add_parking_boy_given_parkingManager_add_parking_boy_to_management_list(){
         // Given
-        List<ParkingLot> parkingLots1 = Arrays.asList(new ParkingLot(10),new ParkingLot(10));
-        List<ParkingLot> parkingLots2 = Arrays.asList(new ParkingLot(10),new ParkingLot(15));
-        List<ParkingLot> parkingLots3 = Arrays.asList(new ParkingLot(15),new ParkingLot(10));
-        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLots1);
-        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLots1);
-        SmartParkingBoy smartParkingBoy1 = new SmartParkingBoy(parkingLots2);
-        SmartParkingBoy smartParkingBoy2 = new SmartParkingBoy(parkingLots2);
-        SuperSmartParkingBoy superSmartParkingBoy1 = new SuperSmartParkingBoy(parkingLots3);
-        SuperSmartParkingBoy superSmartParkingBoy2 = new SuperSmartParkingBoy(parkingLots3);
 
-        List<ParkingBoy> parkingBoyList = Arrays.asList(parkingBoy1, parkingBoy2);
-        List<SmartParkingBoy> smartParkingBoyList = Arrays.asList(smartParkingBoy1,smartParkingBoy2);
-        List<SuperSmartParkingBoy> superSmartParkingBoyList = Arrays.asList(superSmartParkingBoy1,superSmartParkingBoy2);
 
         // When
         ParkingManager parkingManger = new ParkingManager(new ManagementList(parkingBoyList, smartParkingBoyList, superSmartParkingBoyList));
