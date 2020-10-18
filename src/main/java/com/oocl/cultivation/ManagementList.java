31cd8102346191276887fc4a4e356f9922010e5b
TAYAGCH <tayagch@oocl.com>
2020-10-18 22:00:33 +0800

    [ParkingLot][Christian] test : apply new test for case when parking manager add parking then return management list

--- src/main/java/com/oocl/cultivation/ManagementList.java
+++ src/main/java/com/oocl/cultivation/ManagementList.java
@@ -11,6 +11,33 @@ public class ManagementList {
         this.parkingBoyList = parkingBoyList;
         this.smartParkingBoyList = smartParkingBoyList;
         this.superSmartParkingBoyList = superSmartParkingBoyList;
+    }
 
+    public ManagementList() {
+
+    }
+
+    public List<ParkingBoy> getParkingBoyList() {
+        return parkingBoyList;
+    }
+
+    public void setParkingBoyList(List<ParkingBoy> parkingBoyList) {
+        this.parkingBoyList = parkingBoyList;
+    }
+
+    public List<SmartParkingBoy> getSmartParkingBoyList() {
+        return smartParkingBoyList;
+    }
+
+    public void setSmartParkingBoyList(List<SmartParkingBoy> smartParkingBoyList) {
+        this.smartParkingBoyList = smartParkingBoyList;
+    }
+
+    public List<SuperSmartParkingBoy> getSuperSmartParkingBoyList() {
+        return superSmartParkingBoyList;
+    }
+
+    public void setSuperSmartParkingBoyList(List<SuperSmartParkingBoy> superSmartParkingBoyList) {
+        this.superSmartParkingBoyList = superSmartParkingBoyList;
     }
 }
--- src/test/java/com/oocl/cultivation/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -10,51 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingManagerTest {
 
-    List<ParkingLot> parkingLots1;
-    List<ParkingLot> parkingLots2;
-    List<ParkingLot> parkingLots3;
-    ParkingBoy parkingBoy1;
-    ParkingBoy parkingBoy2;
-    SmartParkingBoy smartParkingBoy1;
-    SmartParkingBoy smartParkingBoy2;
-    SuperSmartParkingBoy superSmartParkingBoy1;
-    SuperSmartParkingBoy superSmartParkingBoy2;
-
-    List<ParkingBoy> parkingBoyList;
-    List<SmartParkingBoy> smartParkingBoyList;
-    List<SuperSmartParkingBoy> superSmartParkingBoyList;
-
-    public void setup(){
-        parkingLots1 = Arrays.asList(new ParkingLot(10),new ParkingLot(10));
-        parkingLots2 = Arrays.asList(new ParkingLot(10),new ParkingLot(15));
-        parkingLots3 = Arrays.asList(new ParkingLot(15),new ParkingLot(10));
-
-        parkingBoy1 = new ParkingBoy(parkingLots1);
-        parkingBoy2 = new ParkingBoy(parkingLots1);
-
-        smartParkingBoy1 = new SmartParkingBoy(parkingLots2);
-        smartParkingBoy2 = new SmartParkingBoy(parkingLots2);
-
-        superSmartParkingBoy1 = new SuperSmartParkingBoy(parkingLots3);
-        superSmartParkingBoy2 = new SuperSmartParkingBoy(parkingLots3);
-
-    }
-
     @Test
     public void should_return_management_list_when_parkingManager_add_parking_boy_given_parkingboy_list(){
         // Given
-        setup();
-        parkingBoyList = Arrays.asList(parkingBoy1, parkingBoy2);
-        smartParkingBoyList = Arrays.asList(smartParkingBoy1,smartParkingBoy2);
-        superSmartParkingBoyList = Arrays.asList(superSmartParkingBoy1,superSmartParkingBoy2);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLot1);
+        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLot2);
+        List<ParkingBoy> parkingBoys = Arrays.asList(parkingBoy1, parkingBoy2);
 
         // When
-        ParkingManager parkingManger = new ParkingManager(new ManagementList(parkingBoyList, smartParkingBoyList, superSmartParkingBoyList));
+        ManagementList managementList = new ManagementList();
+        ParkingManager parkingManager = new ParkingManager(managementList);
+        parkingManager.getManagementList().setParkingBoyList(parkingBoys);
 
         // Then
-        assertNotNull(parkingManger.getManagementList());
-        assertSame(parkingBoy1,parkingBoyList.get(0));
-        assertSame(smartParkingBoy1,smartParkingBoyList.get(0));
-        assertSame(superSmartParkingBoy1,superSmartParkingBoyList.get(0));
+        assertSame(managementList.getParkingBoyList(),parkingManager.getManagementList().getParkingBoyList());
     }
 }
